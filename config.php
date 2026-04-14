99535b4c16ddef98f0495d4b03b13025d08ff63b
Francesco Antonio Mangiacotti <167759255+cottiFra@users.noreply.github.com>
2026-04-14 13:21:30 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mammita');
+
+function db_connect() {
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    if (!$conn) {
+        die('Connessione al database fallita: ' . mysqli_connect_error());
+    }
+    mysqli_set_charset($conn, 'utf8mb4');
+    return $conn;
+}
