68c7b141bf4947b1c5028726a8f6a4c56340aefb
Alberto D'Addetta <149900238+albydaddy22@users.noreply.github.com>
2026-04-28 11:11:06 +0200

    Delete UniBank directory

--- UniBank/database(sql)/insertMateriaPerFacolta.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO materiaperfacolta (id_materia, id_facolta) VALUES
-(1, 48),
-(2, 48),
-(3, 48);
--- UniBank/database(sql)/insertMaterie.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO materia (nome) VALUES
-('Database e Normalizzazione'),
-('NoSQL e Database Non Relazionali'),
-('Transazioni e Controllo della Concorrenza');
